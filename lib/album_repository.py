005483c8a1eb3d1b110421c3d8729595b893751a
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-20 16:55:19 +0000

    Adds method docstrings

--- lib/album_repository.py
+++ lib/album_repository.py
@@ -1,3 +1,4 @@
+
 '''
 Defines the AlbumRepository class
 '''
@@ -5,13 +6,22 @@ from lib.album import Album
 
 class AlbumRepository:
     def __init__(self, connection):
+        '''
+        Inits AlbumRepository class with connection to database.
+        '''
         self._connection = connection
 
     def all(self):
+        '''
+        Method to get all rows from the albums table.
+        '''
         rows = self._connection.execute("SELECT * FROM albums;")
         return [Album(row["id"], row["title"], row["release_year"], row["artist_id"]) for row in rows]
 
     def find(self, id):
+        '''
+        Method to get a single row from the albums table, reflecting a given ID
+        '''
         rows = self._connection.execute("SELECT * FROM albums WHERE id = %s;", [id])
         return [Album(row["id"], row["title"], row["release_year"], row["artist_id"]) for row in rows]
 
