5f8c1bbceca86094ba8218f40f68ffbf9f76db13
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-20 16:51:40 +0000

    Implements create method in AlbumRepository class

--- lib/album_repository.py
+++ lib/album_repository.py
@@ -14,3 +14,11 @@ class AlbumRepository:
     def find(self, id):
         rows = self._connection.execute(f"SELECT * FROM albums WHERE id = {id};")
         return [Album(row["id"], row["title"], row["release_year"], row["artist_id"]) for row in rows]
+
+    def create(self, album):
+        '''
+        Method to create (add) an album to the albums table.
+        '''
+        self._connection.execute("INSERT INTO albums (id, title, release_year, artist_id) VALUES (%s, %s, %s, %s)", [album.id, album.title, album.release_year, album.artist_id])
+        return None
+
