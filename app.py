507f22395360caea6d7bab9eeeadb353fbe8d9e4
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-21 09:53:53 +0000

    Updates application interface

--- app.py
+++ app.py
@@ -1,31 +1,47 @@
-from lib.database_connection import DatabaseConnection
+'''
+Entry point to music library application.
+'''
 
+from lib.database_connection import DatabaseConnection
 from lib.artist_repository import ArtistRepository
 from lib.album_repository import AlbumRepository
 
-# Connect to the database
-connection = DatabaseConnection()
-connection.connect()
+class Application:
+    '''
+    Runs the music library application
+    '''
+    def __init__(self) -> None:
+        '''
+        Initialises the connection to the database
+        and seeds the database with data
+        '''
+        # Connect to the database
+        self._connection = DatabaseConnection()
+        self._connection.connect()
+        # Seed with some seed data
+        self._connection.seed("seeds/music_library.sql")
 
-# Seed with some seed data
-connection.seed("seeds/music_library.sql")
+    def run(self) -> None:
+        '''
+        Retrieves artists and albums from the database
+        Prints all artists and albums to the terminal.
+        '''
+        # Retrieve all artists
+        artist_repository = ArtistRepository(self._connection)
+        artists = artist_repository.all()
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
+        # List them out
+        for artist in artists:
+            print(f"{artist.id}: {artist.name} ({artist.genre})")
 
-# List them out
-for artist in artists:
-    print(artist)
+        # Retrieve all albums
+        album_repository: AlbumRepository = AlbumRepository(self._connection)
+        albums = album_repository.all()
 
-# Retrieve all albums
-album_repository = AlbumRepository(connection)
-albums = album_repository.all()
+        # List the albums
+        for album in albums:
+            print(album)
 
-# List the albums
-for album in albums:
-    print(album)
-
-# Retrieve all albums
-single_albums = album_repository.find(1)
-print(single_albums)
+if __name__ == '__main__':
+    app = Application()
+    app.run()
