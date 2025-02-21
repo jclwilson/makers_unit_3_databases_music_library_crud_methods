f0310f52890ee99a5320dd18bb29170a862760ad
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-21 10:23:59 +0000

    Implements CLI following from earlier diary project

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 Entry point to music library application.
 '''
 
+from os import system, name
+import sys
+
 from lib.database_connection import DatabaseConnection
 from lib.artist_repository import ArtistRepository
 from lib.album_repository import AlbumRepository
@@ -21,26 +24,85 @@ class Application:
         # Seed with some seed data
         self._connection.seed("seeds/music_library.sql")
 
-    def run(self) -> None:
+    def clear(self) -> None:
         '''
-        Retrieves artists and albums from the database
-        Prints all artists and albums to the terminal.
+        Function to clear terminal screen.
         '''
-        # Retrieve all artists
+        if name == 'nt':
+            _ = system('cls')
+        else:
+            _ = system('clear')
+
+
+    def continue_menu(self) -> ([''] or None):
+        '''
+        Displays menu asking whether to continue / quit
+        '''
+        choice = ''
+        while choice == '':
+            print('\n\nWhat do you want to do?\n')
+            print('\tm) Go to main menu')
+            print('\n\tq) Quit')
+            choice = input('\nChoose an option:\n\t')
+            match choice:
+                case 'q':
+                    sys.exit()
+                case 'm':
+                    return ''
+                case _:
+                    choice = ''
+
+    def print_artists(self) -> None:
+        '''
+        Prints all artists to the terminal
+        '''
+        self.clear()
         artist_repository = ArtistRepository(self._connection)
         artists = artist_repository.all()
 
-        # List them out
+        print('All artists:\n')
         for artist in artists:
-            print(f"{artist.id}: {artist.name} ({artist.genre})")
+            print(f"\t{artist.id}: {artist.name} ({artist.genre})")
+        return None
 
-        # Retrieve all albums
+    def print_albums(self)-> None:
+        '''
+        Prints all albums to the terminal
+        '''
+        self.clear()
         album_repository: AlbumRepository = AlbumRepository(self._connection)
         albums = album_repository.all()
 
-        # List the albums
+        print('All albums:\n')
         for album in albums:
-            print(album)
+            print(f"\t{album.id}: {album.title} ({album.release_year})")
+        return None
+
+    def run(self) -> None:
+        '''
+        Runs application
+        '''
+        choice = ''
+        while choice == '':
+            self.clear()
+            print('Welcome to the music library manager\n\n')
+            print('What would you like to do?')
+            print('\t1) List all albums')
+            print('\t2) List all artists')
+            print('\n\tq) Quit')
+            choice = input('\nChoose an option:\n\t')
+            match choice:
+                case '1':
+                    self.print_albums()
+                    choice = self.continue_menu()
+                case '2':
+                    self.print_artists()
+                    choice = self.continue_menu()
+                case 'q':
+                    sys.exit()
+                case _:
+                    choice = ''
+
 
 if __name__ == '__main__':
     app = Application()
